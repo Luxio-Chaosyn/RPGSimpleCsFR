24ecfe1186037d4aa9b535a60816fc691715af9f
Luxio-Chaosyn <lacendrelame@outlook.fr>
2023-11-14 14:14:55 +0100

    Adding Rng file

--- RPGSimpleCsFR/Program.cs
+++ RPGSimpleCsFR/Program.cs
@@ -1,4 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+namespace RPGSimpleCsFR
+{
+    public class SimpleRPG
+    {
+        public int startGame()
+        {
+            Character player;
+            Labyrinth maze;
+
+            return (0);
+        }
+    }
+}
 Console.WriteLine("Hello, World!");
-
-
---
+++ RPGSimpleCsFR/Rng.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGSimpleCsFR
+{
+    public static class Rng
+    {
+        private static Random rand = new Random();
+        public static int Roll(int min, int max)
+        {
+            return rand.Next(min, max);
+        }
+    }
+}

7f054a6c5700f3d08a2de183adcadb7efe4e0949
Clement <clement.darribeau@gmail.com>
2023-11-14 14:39:36 +0100

    add observe class and modify genereate class

--- RPGSimpleCsFR/Labyrinth.cs
+++ RPGSimpleCsFR/Labyrinth.cs
@@ -6,14 +6,44 @@ using System.Threading.Tasks;
 
 namespace RPGSimpleCsFR
 {
-    internal abstract class Labyrinth
+    internal class Labyrinth
     {
         // créer le labyrinthe, commence par faire une grille pleine de # puis au harzard enlève 3/5 en les remplaçant par des espace
-        public abstract void Generate(int x, int y);
+        public void Generate(int x, int y)
+        {
+            char[,] maze = new char[x, y];
+            
+            for (int i = 0; i < maze.GetLength(0); i++)
+            {
+                for (int j = 0; j < maze.GetLength(1); j++)
+                {
+                    maze[x, y] = '#';
+                }
+
+
+            }
+
+
+        }
 
         // au hazard rajoute nb 'M' dans le labyrinthe
-        public abstract void Populate(int nb);
+        public void Populate(int nb)
+        {
+
+
+        }
+
         // au hazard rajoute nb 'T' dans le labyrinthe
-        public abstract void Furbish(int nb);
+        public  void Furbish(int nb)
+        {
+
+
+        }
+
+        //renvoie la nature de la case demandé
+        public void observe(int x, int y) { 
+        
+        }
+
     }
 }
